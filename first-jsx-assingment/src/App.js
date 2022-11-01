ed1fbc5747a8fb2d5f590dd61ba22ba9bbb644ec
Dillon Hughes <dillonhughes29@gmail.com>
2022-11-01 14:10:44 -0400

    first commit

---
+++ first-jsx-assingment/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Hello Dojo</h1>
+     <h2> Things I need to do</h2>
+     <ul>
+        <li>Learn react</li>
+        <li>Learn Unity</li>
+        <li>Make my FPS Game</li>
+        <li>Go To Gym</li>
+     </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ prop-it-up-assignment/src/componets/PersonCard.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+class PersonCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      addAge: this.props.age,
+    };
+  }
+
+  render() {
+    const { firstName, lastName, age, hairColor } = this.props;
+
+    return (
+      <React.Fragment>
+        <div>
+          <h1>
+            {lastName}, {firstName}
+          </h1>
+          <p>Age: {this.state.addAge}</p>
+          <p>Hair Color: {hairColor}</p>
+        </div>
+        <button
+          onClick={() => {
+            this.setState({ addAge: this.state.addAge + 1 });
+          }}
+        >
+          Birthday Button for {firstName} {lastName}
+        </button>
+        
+      </React.Fragment>
+    );
+  }
+}
+
+export default PersonCard;
